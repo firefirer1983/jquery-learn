06f7273e04ea4f533e541e30476d8a552b182216
xy <zhangxuyi@wanshare.com>
2019-11-30 22:45:21 +0800

    push

--- bb.js
+++ bb.js
@@ -1,24 +1,37 @@
-const Record = Backbone.Model.extend({})
+// import {LocalStorage} from 'backbone.localstorage';
 
-record = new Record({credit: "CASH", debit:"DEPOSIT"})
-console.log(record.toJSON());
-console.log(record)
+const app = {};
 
-
-const RecordView = Backbone.View.extend({})
-var AppView = Backbone.View.extend({
-  // el - stands for element. Every view has a element associate in with HTML
-  //      content will be rendered.
-  el: '#container',
-  // It's the first function called when this view it's instantiated.
-  initialize: function(){
-    this.render();
-  },
-  // $el - it's a cached jQuery object (el), in which you can use jQuery functions
-  //       to push content. Like the Hello World in this case.
-  render: function(){
-    this.$el.html("Hello World");
+app.Todo = Backbone.Model.extend({
+  defaults: {
+    title: "",
+    finished: false
   }
 });
 
-new AppView()
+todo1 = new app.Todo({ title: "Have a baby", finished: false });
+console.log(Backbone.LocalStorage);
+
+app.TodoList = Backbone.Collection.extend({
+  model: app.Todo,
+  localStorage: new Backbone.LocalStorage("todo-list-storage")
+})
+
+const TodoView = Backbone.View.extend({
+  // el - stands for element. Every view has a element associate in with HTML
+  //      content will be rendered.
+  el: "#container",
+  // It's the first function called when this view it's instantiated.
+  initialize: function() {
+    this.render();
+  },
+  template: _.template("<h1><%=Greeting%></h1>"),
+  // $el - it's a cached jQuery object (el), in which you can use jQuery functions
+  //       to push content. Like the Hello World in this case.
+  render: function() {
+    this.$el.html(this.template({ Greeting: "HELLO MY BABY" }));
+  }
+});
+let todoList = new app.TodoList()
+todoList.create({title: "Be A FullStack"})
+new TodoView();
